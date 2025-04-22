f72b7abc4eac4dab9c3301e4ae5e5e8b5fe48c5b
Sujal Singh <email.sujalsingh@gmail.com>
2025-04-22 08:15:02 +0530

    init wifi

--- firmware/src/main.cpp
+++ firmware/src/main.cpp
@@ -1,6 +1,31 @@
 #include <Arduino.h>
+#include "WiFi.h"
+
+// WiFi
+#ifndef WIFI_SSID
+#define WIFI_SSID "SSID"
+#endif
+#ifndef WIFI_PASSWORD
+#define WIFI_PASSWORD "PASSWORD"
+#endif
+
+void initWiFi() {
+    WiFi.mode(WIFI_STA);
+    WiFi.begin(WIFI_SSID, WIFI_PASSWORD);
+    Serial.print("Connecting to WiFi");
+
+    while (WiFi.status() != WL_CONNECTED) {
+        Serial.print(".");
+        delay(500);
+    }
+    Serial.print("\nConnected: ");
+    Serial.println(WiFi.localIP());
+}
+
 void setup() {
-// write your initialization code here
+    Serial.begin(115200);
+
+    initWiFi();
 }
 
 void loop() {
